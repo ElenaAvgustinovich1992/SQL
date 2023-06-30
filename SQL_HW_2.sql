e52860a5e4b45d711b30255b5562e805e02ea542
Elena Avgustinovich1992 <lena.leno430@gmail.com>
2023-06-30 17:04:45 +0300

    add file sql 2.1

--- SQL_HW_2.sql
+++ SQL_HW_2.sql
@@ -79,39 +79,84 @@ select name from students where created_on ='2021-07-12 00:00:00' and name like 
 select name from students where created_on ='2021-07-12 00:00:00' and name like '%8%';
 
 
-17. 
+17. Diplay users whose id is equal to 110:
 
 select * from students where id='110';
 
+
+18. Diplay users whose id is equa to  153:
+
 select * from students where id='153';
+
+
+19. Diplay users whose id is greater than 140:
 
 select * from students where id>'140';
 
+
+20. Diplay users whose id is less than 130:
+
 select * from students where id<'130';
+
+
+21. Diplay users whose id is less than 127 and greater than 188:
 
 select * from students where id<'127' or id>'188';
 
+
+22. Diplay users whose id is less than or equal to 137:
+
 select * from students where id<='137';
+
+
+23. Diplay users whose id is greater than or equal to 137:
 
 select * from students where id>='137';
 
+
+24. Diplay users whose id is greater than 180 and less than 190:
+
 select * from students where id>'180' and id<'190';
+
+
+25. Diplay users whose id is between 180 and 190:
 
 select * from students where id between 180 and 190;
 
+
+26. Diplay users whose password is equal to 12333, 1m313, 123313:
+
 select * from students where password in ('12333','1m313', '123313');
+
+
+27. Diplay users who were creatrd on 2020-10-03 00:00:00, 2021-05-19 00:00:00, 2021-03-26 00:00:00:
 
 select * from students where created_on in ('2020-10-03 00:00:00','2021-05-19 00:00:00', '2021-03-26 00:00:00');
 
+
+28. Display id is min:
+
 select min(id) from students;
+
+
+29. isplay id is max:
 
 select max(id) from students;
 
+
+30. Display the user count:
+
 select count(id) from students;
+
+
+31. Display id, name and creation date. Sort by the ascending order of user creation date. 
 
 select id, name, created_on from students order by created_on asc;
 
-select id, name, created_on from students order by created_on desc;
+
+32. Display id, name and creation date. Sort by the descending order of user creation date.:
+
+select id, name, created_on from students order by created_on desc
 
 
 
